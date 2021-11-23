a843fbc1da1c1916ca6ad61b9b0bef636b6d054e
hrithik098 <hrithiky328@gmail.com>
2021-11-24 01:39:22 +0530

    Added new methods for wifi handling

--- wifi.py
+++ wifi.py
@@ -8,16 +8,19 @@ class Wifi:
 
     def __init__(self):
         self.ssid = None
+        self.wifi_details = []
 
     
     @classmethod
     def run_command(cls, cmd):
         """
-        Returns a list of all cells extracted from the output of iwlist.
+        Run any command accepted by nmcli.
+        cmd should be a array of strings.
+        for eg: ["sudo", "nmcli", "dev", "wifi"]
         """
         nmcli_result = None
         try:
-            nmcli_result = wrap.check_output(['sudo','nmcli', cmd],stderr=wrap.STDOUT)
+            nmcli_result = wrap.check_output(cmd,stderr=wrap.STDOUT)
         except wrap.CalledProcessError as e:
             print(f"There was some error -> {e.output.strip()}")
         else:
@@ -25,6 +28,16 @@ class Wifi:
         # nmcli_result = nmcli_result.split('\n')
         
         return nmcli_result
+
+    @classmethod
+    def connect(self, ssid, password):
+        """
+        Connect to a network.
+        """
+        cmd = ["sudo", "nmcli", "dev", "wifi", "connect", f"{ssid}", "password", f"{password}"]
+        self.run_command(cmd)
+        self.ssid = ssid
+        return
 
     @classmethod
     def get_connection_status(self):
@@ -36,16 +49,11 @@ class Wifi:
                 "DEVICE": "wlan0",
                 "STATE": "connected",
                 "TYPE": "wifi",
-            },
-            {
-                "CONNECTION": "--",
-                "DEVICE": "p2p-dev-wlan0",
-                "STATE": "disconnected",
-                "TYPE": "wifi-p2p",
-            },
+            }
         ]
         """
-        res = self.run_command("d")
+        cmd = ["sudo", "nmcli", "dev"]
+        res = self.run_command(cmd)
         formatted_data = prepare_data(res)
         return formatted_data
     
@@ -54,18 +62,55 @@ class Wifi:
         """
         Returns True if the wifi is enabled.
         """
-        res =  self.run_command("n")
-        print(dir(res))
+        cmd = ["sudo", "nmcli", "n"]
+        res =  self.run_command(cmd)
         return True if res.find("enabled") != -1 else False
+
+    @classmethod
+    def start_wifi(self):
+        """
+        Starts the wifi.
+        """
+        cmd = ["sudo", "nmcli", "radio", "wifi", "on"]
+        self.run_command(cmd)
+        return True if self.wifi_enabled() else False
+
+    @classmethod
+    def scan(self):
+        """
+        Scans for available networks.
+        """
+        cmd = ["sudo", "nmcli", "dev", "wifi"]
+        res = self.run_command(cmd)
+        formatted_data = prepare_data(res)
+        return formatted_data
 
     @classmethod
     def get_ssid(self):
         """
         Returns the SSID of the current network.
         """
-
         return self.ssid
 
+    @classmethod
+    def get_saved_connections(self):
+        """
+        Returns a list of all saved connections.
+
+        [
+            {
+            'DEVICE': 'wlan0',
+            'NAME': 'WillowCove',
+            'TYPE': 'wifi',
+            'UUID': 'fa06ef2a-d174-4f37-8f95-d12b2117a288'
+            }
+        ]
+
+        """
+        cmd = ["sudo", "nmcli", "c"]
+        res = self.run_command(cmd)
+        formatted_data = prepare_data(res)
+        return formatted_data
 
 
 def prepare_data(data):
@@ -77,17 +122,42 @@ def prepare_data(data):
     p2p-dev-wlan0  wifi-p2p  disconnected  --
     lo             loopback  unmanaged     --
 
+    >> 
+        [
+            {
+                "CONNECTION": "WillowCove",
+                "DEVICE": "wlan0",
+                "STATE": "connected",
+                "TYPE": "wifi",
+            },
+            { ... },
+            { ... },
+        ]
+
     """
     final_data = []
 
     rows = data.split("\n")
+    # after splitting the data, we have a empty array in the end.
+    del rows[-1]
     temp_data = []
     for row in rows:
             single_row = row.split(" ")
+            print("##########################################################################################")
+            print(single_row)
+            print("##########################################################################################")
             formatted_data = []
-            for ele in single_row:
-                    if (ele != ""):
-                            formatted_data.append(ele)
+            for ele in range(0,len(single_row) - 1):
+                    if (single_row[ele] != "" or ele == 0):
+                            val = single_row[ele]
+                            last_index = len(single_row) - 1
+                            # if 2 consecutive elements in the row are not empty, then we have to combine them.
+                            # For eg: "120","MBits/s"
+                            if (ele != last_index):
+                                if (single_row[ele+1] != ""):
+                                        val = val  + " " + single_row[ele+1]
+                            formatted_data.append(val)
+            # Remove the last element which is always empty.
             if len(formatted_data) > 0:
                 temp_data.append(formatted_data)
 
@@ -96,6 +166,9 @@ def prepare_data(data):
     for j in range(1,length_final_data):
         temp= {}
         for i in range(0,length_final_data):
+            # Preparing the data. Traverse the temp_data (2D array) and create a dict.
+            # For eg: temp_data = [["DEVICE", "TYPE", "STATE", "CONNECTION"], ["wlan0", "wifi", "connected", "WillowCove"]]
+            # index[0][n] contains the keys and index [1][n], [2][n], [3][n]... contains the values.
             temp[f"{temp_data[0][i]}"] = temp_data[j][i]
         
         final_data.append(temp)
